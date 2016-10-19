243eb11a83dae50cb58f373847c71aa055436b5f
Miguel <miguel.o.herrera@gmail.com>
2016-10-19 14:34:06 -0500

    Initial setup

---
+++ ceasar_cipher.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+
+get '/' do
+  erb: index
+end
+
+post '/' do
+  @string = caesar_cipher(params[:text], 5)
+  erb: index
+end
+
+def caesar_cipher(string, shift_factor)
+  lower = 'abcdefghijklmnopqrstuvwxyz'
+  upper = lower.upcase
+
+  string.length.times do |p|
+  
+    # if current letter is lowercase 
+    string[p] = lower[(lower.index(string[p]) + shift_factor) % 26] if lower.include? string[p]
+    # if current letter is uppercase
+    string[p] = upper[(upper.index(string[p]) + shift_factor) % 26] if upper.include? string[p]
+  
+  end
+  # returns the modified string
+  string
+
+end
