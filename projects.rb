88ba1c3f4b372b781d0e239e5ec712f75ede6e9b
Miguel <miguel.o.herrera@gmail.com>
2016-10-31 20:33:44 -0600

    Check entered letter

--- projects.rb
+++ projects.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
+
+enable :sessions
 
 get '/' do
   erb :index
@@ -14,14 +17,22 @@ post '/caesar_cipher' do
 end
 
 get '/hangman' do
-  @secret_word = setup_game
+  setup_game
   erb :hangman
 end
 
 post '/hangman' do
-  @letter = params[:letter]
+  @letter = params[:letter].upcase
+  @error_message = check_letter(@letter)
+  #session[:used_words] << @letter
+  @secret_word = session[:secret_word]
+  @letters = session[:used_words]
+  @show_word = session[:show_word]
+  @turn = session[:turn]
   erb :hangman
 end
+
+########### Caesar Cipher ############
 
 def caesar_cipher(string, shift_factor)
   lower = 'abcdefghijklmnopqrstuvwxyz'
@@ -40,96 +51,57 @@ def caesar_cipher(string, shift_factor)
 
 end
 
-require 'yaml'
+########### Hangman ############
 
-class Hangman
-  
-  def initialize(words)
-    @words = words
-    start
-  end
-
-  private
-  def play
-    @hidden_word = @words.sample.upcase.split('')
-    @used_words = []
-
-    @show_word =  ("_" * @hidden_word.length).split('')
-    puts "#{@show_word.join(' ')}"
-    @turn = 8
-    while @turn > 0  
-      t = read_turn
-      save_game if t == "#"
-
-      if @hidden_word.include? t
-        @hidden_word.each_with_index { |x, i| @show_word[i] = x if @hidden_word[i] == t}
-        if won?
-          puts "---> #{@show_word.join} <---"
-          puts "\n* * * * * *  You Won!  * * * * * *"
-          break
-        end  
-      else
-        @turn -= 1
-      end
-      puts "#{@show_word.join(" ")}     Turns left: #{@turn}     Words used: #{@used_words.join(" ")}"
-    end
- 
-    puts "\nYou lost!  The secret word was: #{@hidden_word.join}\n" if !won? 
-    play_again? ? start : exit
-  end
-  
-  def play_again?
-    print "\nDo you want to play again (y/n)?: "
-    gets.chomp.upcase == "Y"? true : false
-  end
-  
-  def won?
-    @hidden_word == @show_word
-  end
-  
-  def read_turn
-    while true
-      print "\n(To save the game enter '#' )    Guess a letter: "
-      @guess = gets.chomp.upcase
-      if @guess.length != 1
-        puts "\nType only one character and press enter..."
-      elsif "ABCDEFGHIJKLMNOPQRSTUVWXYZ#".include? @guess 
-        if @show_word.include? @guess
-          puts "\nWord already used, please try another one."
-        else
-          break
-        end
-      else
-        puts "\nInvalid entry, please try again...!"
-      end
-    end  
-    @used_words << @guess if @guess != "#"
-    puts @guess
-    @guess
-  end
-
-  def start
-    puts "\n... Play Hangman ..."
-    puts "  1. New Game"
-    puts "  2. Load Game"
-    puts "  3. Exit"
-    print "Choose an option from 1 to 3: "
-    option = gets.chomp.to_i
-    case option
-    when 1
-      play
-    when 2
-      load_game
-    when 3
-      exit
-    end
-  end
-  
-end
 
 def setup_game
   words = []
   all_words = File.readlines("5desk.txt")
   all_words.each { |w| words << w.strip if w.strip.length > 4 && w.strip.length < 13 }
-  words.sample
+  session[:secret_word] = words.sample.upcase
+  session[:used_words] = []
+  session[:show_word] =  ("_" * session[:secret_word].length).split('')
+  session[:turn] = 8
+end
+
+def check_letter(letter)
+  if letter.length != 1
+    return "Type only one character..."
+  elsif "ABCDEFGHIJKLMNOPQRSTUVWXYZ".include? letter 
+    if session[:show_word].include? letter
+      return "Word already used, please try another one."
+    else
+      word = session[:secret_word].split('')
+      word.each_with_index { |x, i| session[:show_word][i] = x if word[i] == letter}
+      session[:used_words] << letter
+      return letter
+    end
+  else
+    return "Invalid entry, please try again...!"
+  end  
+end
+
+def play
+
+  while @turn > 0  
+    t = read_turn
+
+    if @hidden_word.include? t
+      @hidden_word.each_with_index { |x, i| @show_word[i] = x if @hidden_word[i] == t}
+      if won?
+        puts "---> #{@show_word.join} <---"
+        puts "\n* * * * * *  You Won!  * * * * * *"
+        break
+      end  
+    else
+      @turn -= 1
+    end
+    puts "#{@show_word.join(" ")}     Turns left: #{@turn}     Words used: #{@used_words.join(" ")}"
+  end
+
+  puts "\nYou lost!  The secret word was: #{@hidden_word.join}\n" if !won? 
+end
+
+def won?
+  @hidden_word == @show_word
 end
