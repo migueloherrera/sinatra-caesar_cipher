eb6d3ecf9b26af84167118626833cdd27542e65a
Miguel <miguel.o.herrera@gmail.com>
2016-11-02 20:17:58 -0600

    Finish game logic

--- projects.rb
+++ projects.rb
@@ -18,13 +18,13 @@ end
 
 get '/hangman' do
   setup_game
+  @show_word = session[:show_word]
   erb :hangman
 end
 
 post '/hangman' do
   @letter = params[:letter].upcase
   @error_message = check_letter(@letter)
-  #session[:used_words] << @letter
   @secret_word = session[:secret_word]
   @letters = session[:used_words]
   @show_word = session[:show_word]
@@ -68,40 +68,24 @@ def check_letter(letter)
   if letter.length != 1
     return "Type only one character..."
   elsif "ABCDEFGHIJKLMNOPQRSTUVWXYZ".include? letter 
-    if session[:show_word].include? letter
+    if (session[:show_word].include? letter) || (session[:used_words].include? letter)
       return "Word already used, please try another one."
+    elsif !session[:secret_word].include? letter
+      session[:used_words] << letter
+      session[:turn] = session[:turn].to_i - 1
+      session[:turn] <= 0 ? "you lost!" : "One less chance"
     else
       word = session[:secret_word].split('')
       word.each_with_index { |x, i| session[:show_word][i] = x if word[i] == letter}
       session[:used_words] << letter
-      return letter
+      won? ? "you won!" : letter
     end
   else
     return "Invalid entry, please try again...!"
   end  
 end
 
-def play
-
-  while @turn > 0  
-    t = read_turn
-
-    if @hidden_word.include? t
-      @hidden_word.each_with_index { |x, i| @show_word[i] = x if @hidden_word[i] == t}
-      if won?
-        puts "---> #{@show_word.join} <---"
-        puts "\n* * * * * *  You Won!  * * * * * *"
-        break
-      end  
-    else
-      @turn -= 1
-    end
-    puts "#{@show_word.join(" ")}     Turns left: #{@turn}     Words used: #{@used_words.join(" ")}"
-  end
-
-  puts "\nYou lost!  The secret word was: #{@hidden_word.join}\n" if !won? 
-end
-
 def won?
-  @hidden_word == @show_word
+  guess = session[:show_word].join
+  session[:secret_word] == guess
 end
